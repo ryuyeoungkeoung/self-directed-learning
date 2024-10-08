f3224783f16327adcf668ba0b3273273c17b2d02
ryuyroungkeoung <yok1812@naver.com>
2024-10-08 16:34:02 +0900

    반응형까지 완료 최종

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // 인풋영역 클릭이벤트
 // 인풋을 갖고옴 클릭하면의 클릭
 const inputBox = document.getElementById("input_box");
@@ -20,6 +22,45 @@ function inputWrap(e) {
 }
 inputBox.onclick = inputWrap;
 //------------------------------------------
+// 클릭당하는 햄버거 버튼
+const menuBox = document.getElementById("menu_btn");
+// 클릭당했을 때 나오는 서브메뉴
+const subMenu = document.querySelector(".menu_wrap");
+
+menuBox.onclick = function (e) {
+  e.preventDefault();
+  subMenu.style.transform = "translateX(0%)";
+};
+
+// 서브메뉴 엑스 버튼 누르면 원래 없어졌던 값으로 돌아가기
+const showBox = document.querySelector(".menu_wrap");
+const btnExit = document.querySelector(".exit_btn");
+
+btnExit.onclick = function () {
+  showBox.style.transform = "translateX(100%)"; // 올바른 transform 속성 사용
+};
+
+// ----------------------------------------
+// // 오른쪽 서브 li누르면 2차 메뉴 스르륵 나오게 하기
+const sub2Menu = document.querySelectorAll(".main_box li");
+
+sub2Menu.forEach((menuItem) => {
+  menuItem.addEventListener("click", function (e) {
+    // 클릭한 메뉴의 2차 메뉴 선택 현재 1차메뉴안에 있는 2차메뉴라서 밖으로 빠질 수 없다.
+    const subMenu = this.querySelector(".sub_twomenu");
+
+    // 2차 메뉴가 열려 있는지 확인
+    if (subMenu.style.height === "0px" || subMenu.style.height === "") {
+      // 2차 메뉴 열기 (앞에 정해준 라인하이트 값120으로 변하게 된다. )
+      subMenu.style.height = `${subMenu.scrollHeight}px`;
+    } else {
+      // 2차 메뉴 닫기
+      subMenu.style.height = "0px";
+    }
+  });
+});
+
+// ----------------------------------------
 
 // 팝업 버튼실행 클릭이벤트
 //팝업버튼
@@ -102,3 +143,31 @@ function filterBtn(e) {
     boxWrap[3].style.display = "block";
   }
 }
+// 슬라이더
+new Swiper(".swiper", {
+  direction: "horizontal", // 슬라이드 진행 방향, 기본값 horizontal(가로)
+
+  // autoplay: true 도 가능하지만 객체 데이터로 할당하면 추가적인 옵션 설정 가능
+  autoplay: {
+    // 자동재생 여부
+    delay: 5000, // 시작시간 설정
+  },
+  loop: true, // 반복재생 여부
+  slidesPerView: 1, // 한번에 보여줄 슬라이드 개수
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+
+  // 페이저 버튼 사용자 설정
+  pagination: {
+    // 페이지 번호 요소 선택자
+    el: ".promotion .swiper-pagination",
+
+    // 사용자의 페이지 번호 요소 제어 가능 요소 (사용자가 단순히 시각적으로만 보는것 뿐만아니라 눌러서 제어할 수 있는지에 대한 여부)
+    clickable: true,
+  },
+
+  // nav 화살표 출력 시 추가
+  navigation: {
+    prevEl: ".prev",
+    nextEl: ".next",
+  },
+});
